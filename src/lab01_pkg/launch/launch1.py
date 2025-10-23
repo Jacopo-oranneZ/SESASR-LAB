8ee26d41066aefb6c71f1d67ea63d17d57d59d95
JacopoZennaro <jacopo.zennaro.1@gmail.com>
2025-10-23 09:06:13 +0000

    reset_node creato e funzionante (solo subscriber)

--- src/lab01_pkg/launch/launch1.py
+++ src/lab01_pkg/launch/launch1.py
@@ -14,5 +14,11 @@ def generate_launch_description():
             executable='localization',
             name='localization_node',
             output='screen'
+        ),
+        Node(
+            package='lab01_pkg',
+            executable='reset_node',
+            name='reset_node',
+            output='screen'
         )
     ])
