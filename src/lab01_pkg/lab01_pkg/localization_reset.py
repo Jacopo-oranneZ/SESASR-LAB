3022f94634f0d08a0c55c43994f9ef7ebe9d7e98
JacopoZennaro <jacopo.zennaro.1@gmail.com>
2025-10-23 09:25:28 +0000

    Creati i nodi resettabili di controller e localization (TERMINE LAB 01)

---
+++ src/lab01_pkg/lab01_pkg/localization_reset.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import rclpy
+import math
+
+from rclpy.node import Node
+from geometry_msgs.msg import Pose, Twist, Quaternion
+from std_msgs.msg import Bool
+
+class LocalizationReset(Node):
+    def __init__(self):
+        super().__init__('localization_reset')
+
+        self.x = 0.0
+        self.y = 0.0
+        self.yaw = 0.0         
+        self.time = 1.0           
+        self.vx_prev = 0.0
+        self.vy_prev = 0.0
+        self.msg_1 = True  
+
+
+        self.subscriber = self.create_subscription(Twist, '/cmd_vel', self.cmd_callback, 10)
+        self.subscriber = self.create_subscription(Bool, '/reset', self.reset_callback, 10)
+        self.publisher = self.create_publisher(Pose, '/pose', 10)
+
+       
+        self.get_logger().info('Nodo Localization inizializzato.')
+
+
+    def reset_callback(self, msg: Bool):
+        if msg.data:
+            self.get_logger().info('Reset ricevuto, ripristino stato iniziale.')
+            self.x = 0.0
+            self.y = 0.0
+            self.yaw = 0.0         
+            self.time = 1.0           
+            self.vx_prev = 0.0
+            self.vy_prev = 0.0
+            self.msg_1 = True
+
+    def cmd_callback(self, msg: Twist):
+        vx = msg.linear.x
+        vy = msg.linear.y
+
+        if not self.msg_1:
+            self.x += self.vx_prev * self.time
+            self.y += self.vy_prev * self.time
+        else:
+            self.msg_1 = False
+
+            self.theta = math.atan2(vy, vx)
+
+        q = Quaternion()
+        q.x = 0.0
+        q.y = 0.0
+        q.z = math.sin(self.theta / 2.0)
+        q.w = math.cos(self.theta / 2.0)
+
+ 
+        pose = Pose()
+        pose.position.x = self.x
+        pose.position.y = self.y
+        pose.position.z = 0.0
+        pose.orientation = q
+
+       
+        self.publisher.publish(pose)
+
+        
+        self.vx_prev = vx
+        self.vy_prev = vy
+
+       
+        self.get_logger().info(
+            f"Pose → x={self.x:.2f}, y={self.y:.2f}, theta={math.degrees(self.theta):.1f}°, "
+            f"quat=({q.x:.2f}, {q.y:.2f}, {q.z:.2f}, {q.w:.2f})"
+        )
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = LocalizationReset()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
